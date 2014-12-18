5e49948b9d7a0e32c22b86bd9345c4ddea7a7695
Daniel Waksman <dwaksman@gmail.com>
2014-12-18 09:34:13 -0200

    CSS Enhance

--- logbook/flights/forms.py
+++ logbook/flights/forms.py
@@ -1,7 +1,11 @@
-from django.forms import ModelForm, modelformset_factory, inlineformset_factory
+from django import forms
+from django.forms import ModelForm, inlineformset_factory
 from flights.models import Flight, FlightLeg
 
 class FlightEntryForm(ModelForm):
+    number = forms.CharField(max_length=254, widget=forms.TextInput(attrs={'placeholder': 'Flight Number'}))
+    flight_origin_ICAO = forms.CharField(max_length=4, widget=forms.TextInput(attrs={'placeholder': 'From ICAO'}))
+    flight_dest_ICAO = forms.CharField(max_length=4, widget=forms.TextInput(attrs={'placeholder': 'To ICAO'}))
 
     class Meta:
             model = Flight
