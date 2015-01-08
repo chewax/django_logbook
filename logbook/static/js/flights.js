11211cfaedc2796b239d342f2d156165023fa3a3
Daniel Waksman <dwaksman@gmail.com>
2015-01-08 17:50:05 -0200

    Refactoring

--- logbook/static/js/flights.js
+++ logbook/static/js/flights.js
@@ -4,8 +4,7 @@ $(".datetimepicker").datetimepicker({
 });
 
 $('#add_more').click(function() {
-    cloneMore('.flight-legs table tr:last', 'flightleg_set');
-
+    clone('.flight-legs table tr:last', 'flightleg_set');
     $(".datetimepicker").datetimepicker({
     format:'Y-m-d H:i',
     mask:true
@@ -16,30 +15,3 @@ $('#add_more').click(function() {
 $('#remove_row').click(function() {
     unClone('.flight-legs table tr:last', 'flightleg_set');
 });
-
-function cloneMore(selector, type) {
-    var newElement = $(selector).clone();
-    var total = $('#id_' + type + '-TOTAL_FORMS').val();
-    newElement.find(':input').each(function() {
-        var name = $(this).attr('name').replace('-' + (total-1) + '-','-' + total + '-');
-        var id = 'id_' + name;
-        $(this).attr({'name': name, 'id': id}).val('').removeAttr('checked');
-    });
-    newElement.find('label').each(function() {
-        var newFor = $(this).attr('for').replace('-' + (total-1) + '-','-' + total + '-');
-        $(this).attr('for', newFor);
-    });
-    total++;
-    $('#id_' + type + '-TOTAL_FORMS').val(total);
-    $(selector).after(newElement);
-}
-
-function unClone(selector, type) {
-    //var newElement = $(selector).clone();
-    var total = $('#id_' + type + '-TOTAL_FORMS').val();
-    if (total > 1) {
-        $(selector).remove();
-        total--;
-        $('#id_' + type + '-TOTAL_FORMS').val(total);
-    }
-}
--- logbook/static/js/log_base.js
+++ logbook/static/js/log_base.js
@@ -7,3 +7,30 @@ $('.drawer-menu').click(function(){
     $('.top-menu').toggleClass('toggle')
     }
 )
+
+function clone(selector, type) {
+    var newElement = $(selector).clone();
+    var total = $('#id_' + type + '-TOTAL_FORMS').val();
+    newElement.find(':input').each(function() {
+        var name = $(this).attr('name').replace('-' + (total-1) + '-','-' + total + '-');
+        var id = 'id_' + name;
+        $(this).attr({'name': name, 'id': id}).val('').removeAttr('checked');
+    });
+    newElement.find('label').each(function() {
+        var newFor = $(this).attr('for').replace('-' + (total-1) + '-','-' + total + '-');
+        $(this).attr('for', newFor);
+    });
+    total++;
+    $('#id_' + type + '-TOTAL_FORMS').val(total);
+    $(selector).after(newElement);
+}
+
+function unClone(selector, type) {
+    //var newElement = $(selector).clone();
+    var total = $('#id_' + type + '-TOTAL_FORMS').val();
+    if (total > 1) {
+        $(selector).remove();
+        total--;
+        $('#id_' + type + '-TOTAL_FORMS').val(total);
+    }
+}
