b7e2ca136fa7c44d377ad936cfb83c7db5f8b1d1
Daniel Waksman <dwaksman@gmail.com>
2015-01-14 09:32:06 -0200

    BugFix: User can only acces its own settings

--- logbook/user_settings/views.py
+++ logbook/user_settings/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.views.generic import UpdateView
 from core.views import CurrencyAsideMixin
 from user_settings.forms import SettingsEntryForm
@@ -9,3 +10,10 @@ class ProcessSettingsView(UpdateView, CurrencyAsideMixin):
     template_name = 'user_settings/settings_base.html'
     success_url = '/dashboard/'
     model = UserSettings
+
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+        if user.settings.pk == int(kwargs['pk']):
+            return super(ProcessSettingsView, self).get(request, *args, **kwargs)
+        else:
+            return redirect('/settings/update/{}'.format(user.settings.pk))
